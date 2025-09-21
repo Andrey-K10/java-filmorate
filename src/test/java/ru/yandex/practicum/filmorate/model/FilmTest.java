7603b9797bca983bac37234ccdbf12d1c70c6726
Andre <andrey-k10@yandex.ru>
2025-09-21 20:57:56 +0400

    Первое изменение

--- src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
